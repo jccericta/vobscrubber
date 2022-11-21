22f295f0913d3ce49fe15fbf62740f1ad695cd9b
John Ericta <jericta@steelgem.com>
2022-11-21 13:58:14 -0800

    initial commit

---
+++ VOBScrubber/Program.fs
@@ -0,0 +1,38 @@
+﻿module PdfScrubber
+open System.Text
+open PdfSharp.Pdf.IO
+open PdfSharp.Pdf.Content
+open PdfSharp.Pdf.Content.Objects
+open FSharp.Markdown
+open FSharp.Markdown.Pdf
+open FSharp.MetadataFormat.ValueReader
+open FSharp.Data
+
+let rec extractText(content:CObject, sb:StringBuilder) =
+   match content with
+   | :? CArray as (xs: CArray) -> for x: CObject in xs do extractText(x, sb)
+   | :? CComment -> ()
+   | :? CInteger -> ()
+   | :? CName -> ()
+   | :? CNumber -> ()
+   | :? COperator as (op: COperator) // Tj/TJ = Show text
+      when op.OpCode.OpCodeName = OpCodeName.Tj ||
+            op.OpCode.OpCodeName = OpCodeName.TJ ->
+      for element: CObject in op.Operands do extractText(element, sb)
+      sb.Append(" ") |> ignore
+   | :? COperator -> ()
+   | :? CSequence as (xs: CSequence) -> for x: CObject in xs do extractText(x, sb)
+   | :? CString as (s: CString) -> sb.Append(s.Value) |> ignore
+   | (x: CObject) -> raise <| System.NotImplementedException(x.ToString())
+
+let readAllText (pdfPath:string) =
+   use document: PdfSharp.Pdf.PdfDocument = PdfReader.Open(pdfPath, PdfDocumentOpenMode.ReadOnly)
+   let result: StringBuilder = StringBuilder()
+   for page: PdfSharp.Pdf.PdfPage in document.Pages do
+      let content: CSequence = ContentReader.ReadContent(page)
+      extractText(content, result)
+      result.AppendLine() |> ignore
+   result.ToString()
+   
+let text: string = readAllText @"../Roberto Retana The Edge VOB 04.12.2022.pdf"
+
---
+++ VOBScrubber/test.fsx
@@ -0,0 +1,85 @@
+#r @"bin\Debug\net7.0\FSharp.Core.dll"
+#r @"bin\Debug\net7.0\PdfSharp.dll"
+#r @"bin\Debug\net7.0\PdfSharp.Charting.dll"
+#r @"bin\Debug\net7.0\FSharp.Markdown.dll"
+#r @"bin\Debug\net7.0\FSharp.Markdown.Pdf.dll"
+#r @"bin\Debug\net7.0\FSharp.MetadataFormat.dll"
+
+open System.Text
+open PdfSharp.Pdf.IO
+open PdfSharp.Pdf.Content
+open PdfSharp.Pdf.Content.Objects
+open FSharp.Markdown
+open FSharp.Markdown.Pdf
+open FSharp.MetadataFormat.Reader
+open FSharp.MetadataFormat.ValueReader
+open FSharp.Data
+
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance)
+
+let path = @"../VOB Hansei.pdf"
+let doc = PdfSharp.Pdf.IO.PdfReader.Open(path, PdfDocumentOpenMode.InformationOnly)
+let form = doc.AcroForm
+let fields = form.Fields
+
+let guid = doc.Guid |> printfn "'Guid': { %A }"
+
+let objStr = System.Text.StringBuilder()
+
+fields.Names
+fields.DescendantNames |> printfn "%A"
+
+let loop (fields:PdfSharp.Pdf.AcroForms.PdfAcroField.PdfAcroFieldCollection) = 
+   (
+      for i = 0 to fields.Count - 1 do
+         let pField = fields.[i]
+         let pFieldName = pField.Name
+         if pField.GetType() <> typeof<PdfSharp.Pdf.AcroForms.PdfCheckBoxField> then
+            if pField.HasKids = true then
+               let cFields = pField.Fields
+               for j = 0 to cFields.Count - 1  do
+                  let cField = cFields.[j]
+                  let cFieldName = cField.Name
+                  let cItem = cFields.Item(cFieldName)
+                  let cItemName = cItem.Name
+                  let cFieldValue = 
+                     if cItem.Value = null then
+                        let result = ""
+                        result
+                     else
+                        let result = cItem.Value.ToString()
+                        result
+                  printfn "'%s': { %s }" cItemName cFieldValue
+            else
+               if pField.Value <> null then
+                  printfn "'%s': { %s }" pFieldName (pField.Value.ToString())
+               else 
+                  printfn "'%s': { %s }" pFieldName null
+         else
+            if pField.Value <> null then
+                  printfn "'%s': { %s }" pFieldName (pField.Value.ToString())
+               else 
+                  printfn "'%s': { %s }" pFieldName null
+   ) |> printfn "} %A"
+   
+
+let rec loop2 (fields:PdfSharp.Pdf.AcroForms.PdfAcroField.PdfAcroFieldCollection) = 
+   let fieldNames = fields.Names
+   for fn in fieldNames do
+      if fields.Item(fn).HasKids then
+         printfn "'%s' : {" (fields.Item(fn).Name)
+         let fieldKids = fields.Item(fn).Fields
+         loop fieldKids
+      else
+         let field = fields.Item(fn).Name
+         if fields.Item(fn).Value <> null then
+            let fieldValue = fields.Item(fn).Value.ToString()
+            printfn "%s: { %s }" field fieldValue
+         else
+            printfn "%s: { %s }" field null
+
+do loop2 fields
+
+
+
+
