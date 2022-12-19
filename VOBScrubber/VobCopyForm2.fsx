0991dc24318d99207beab66d97d7086187087549
John Ericta <jericta87@gmail.com>
2022-12-19 07:06:47 -0800

    branched off for second version of the project

--- VOBScrubber/VobCopyForm2.fsx
+++ VOBScrubber/VobCopyForm2.fsx
@@ -5,6 +5,7 @@
 #r @"bin\Debug\net7.0\itext.forms.dll"
 #r @"bin\Debug\net7.0\itext.kernel.dll"
 #r @"bin\Debug\net7.0\itext.io.dll"
+#r @"bin\Debug\net7.0\itext.layout.dll"
 #r @"bin\Debug\net7.0\itext.commons.dll"
 #r @"bin\Debug\net7.0\itext.pdfa.dll"
 #r @"bin\Debug\net7.0\itext.pdfxfa.dll"
@@ -17,19 +18,20 @@ open iText.Kernel
 open iText.Commons
 open iText.Pdfa
 open iText.Pdfocr
+open iText.Layout
 open Microsoft.FSharp.Collections
 open System.Collections.Generic
 
-let currDir = System.IO.Directory.GetCurrentDirectory()
-let vobSampleFile = currDir + @"\pdfs\Test.pdf"
-let vobSampleReader = new Pdf.PdfReader(vobSampleFile)
-let vobSamplePDF = new Pdf.PdfDocument(vobSampleReader)
+let currDir: string = System.IO.Directory.GetCurrentDirectory()
 let pdfSourceFile = currDir + @"\pdfs\John Ericta TaxReturn 2021.pdf"
 let pdfSourceReader = new Pdf.PdfReader(pdfSourceFile)
 let pdfSource = new Pdf.PdfDocument(pdfSourceReader)
-let pdfTargetFile = new Pdf.PdfWriter(currDir + @"\pdfs\Copy.pdf")
+let pdfTargetFile = currDir + @"\pdfs\Copy.pdf"
 let pdfTargetWriter = new Pdf.PdfWriter(pdfTargetFile)
 let pdfTarget' = new Pdf.PdfDocument(pdfTargetWriter)
+let vobSampleFile = currDir + @"\pdfs\Test.pdf"
+let vobSampleReader = new Pdf.PdfReader(vobSampleFile)
+let vobSamplePDF = new Pdf.PdfDocument(vobSampleReader)
 (*let list:IList<int> = new List<int>()
 let buildList (p:Pdf.PdfDocument) (l:IList<int>) = 
     for i in 1 .. p.GetNumberOfPages() do
@@ -40,28 +42,40 @@ let copiedPages = pdfSource.CopyPagesTo(iList, pdfTarget)*)
 let copyPages (source:Pdf.PdfDocument) (target:Pdf.PdfDocument) = 
     for i in 1 .. source.GetNumberOfPages() do
         target.AddPage(source.GetPage(i).CopyTo(target)) |> ignore
-    let ref = target.CreateNextIndirectReference()
-    ref
+    let rel = target.CreateNextIndirectReference()
+    rel
+    //target
+
 let pdfTarget = copyPages pdfSource pdfTarget'
 let pdfFormCopier = new PdfPageFormCopier()
-let copyPageForm (c:PdfPageFormCopier) (s:Pdf.PdfDocument) (t:Pdf.PdfDocument) = 
-    for i in 1 .. s.GetNumberOfPages() do
+
+let copyPageForm (c:PdfPageFormCopier) (s:Pdf.PdfDocument) (t:Pdf.PdfDocument) =
+    let pdfAFSource: PdfAcroForm = PdfAcroForm.GetAcroForm(s, false)
+    let pdfAFTarget: PdfAcroForm = PdfAcroForm.GetAcroForm(t, true)
+    // Copy the template's form
+    for i: int32 in 1 .. s.GetNumberOfPages() do
         c.Copy(s.GetPage(i),t.GetPage(i))
-    let pdfAcroForm' = PdfAcroForm.GetAcroForm(t, true)
-    let acfFields = pdfAcroForm'.GetFieldsForFlattening()
-    for acfField in acfFields do
-        pdfAcroForm'.AddFieldAppearanceToPage(acfField, t.GetPage(t.GetPageNumber(acfField.GetPdfObject())))
-    pdfAcroForm'.FlattenFields()
-    let pdfXFA = pdfAcroForm'.GetXfaForm()
-    let pdfXFAForm = pdfXFA.Write(pdfAcroForm')
-    t.Close()
-    pdfXFAForm
+    //Get the template's form fields
+    let pdfAFSourceFields = pdfAFSource.GetFormFields()
+    // Copy form fields from the template and add to target
+    for pdfAFSourceField in pdfAFSourceFields do
+        pdfAFTarget.AddField(pdfAFSourceField.Value)
+    let acfTargetFields = pdfAFTarget.GetFieldsForFlattening()
+    // Make them appear on the page
+    for acfTargetField in acfTargetFields do
+        pdfAFTarget.AddFieldAppearanceToPage(acfTargetField, t.GetPage(t.GetPageNumber(acfTargetField.GetPdfObject())))
+    //pdfAFSource.FlattenFields()
+    //let pdfXFA = pdfAFTarget.GetXfaForm()
+    //pdfXFA.Write(pdfAFTarget)
+    // t.Close()
+    t
+    
+let pdfTargetTemplated = copyPageForm pdfFormCopier vobSamplePDF (pdfTarget.GetDocument())
 
-copyPageForm pdfFormCopier vobSamplePDF (pdfTarget.GetDocument())
-
-vobSampleReader.Close()
+let newDoc = new Document(pdfTargetTemplated)
+//vobSampleReader.Close()
+//pdfSourceReader.Close()
+//pdfTargetWriter.Close()
 vobSamplePDF.Close()
-pdfSourceReader.Close()
 pdfSource.Close()
-pdfTargetWriter.Close()
-pdfTarget.GetDocument().Close()
+newDoc.Close()
